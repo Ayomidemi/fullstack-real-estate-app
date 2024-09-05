6394d82de7a469a8cca490af0d3c6503544cfd75
Ayomidemi <adenijiayomide13@gmail.com>
2024-09-05 23:18:16 +0100

    databaase connection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import AboutUs from "./pages/AboutUs";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<Login />} />
